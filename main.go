929676224f4249cd4768eecadf1c0b51fd119bd4
go je <27877342+gocs@users.noreply.github.com>
2020-02-01 17:41:17 +0800

    Update

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"image/color"
+	"log"
+
+	"github.com/hajimehoshi/ebiten"
+)
+
+const (
+	screenWidth  = 640
+	screenHeight = 480
+)
+
+var emptyImage, _ = ebiten.NewImage(16, 16, ebiten.FilterDefault)
+
+func init() {
+	emptyImage.Fill(color.White)
+}
+
+func main() {
+	if err := ebiten.Run(update, screenWidth, screenHeight, 1, "Polygons (Ebiten Demo)"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+var locx float32
+var locy float32
+
+func update(screen *ebiten.Image) error {
+
+	_, dy := ebiten.Wheel()
+	if ebiten.IsKeyPressed(ebiten.KeyShift) {
+		locx -= float32(dy)
+	} else {
+		locy -= float32(dy)
+	}
+
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+
+	atlas := [][]int{
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 4, 12, 8, 0, 0, 0, 0, 0, 0},
+		{0, 6, 0, 9, 0, 0, 0, 0, 0, 0},
+		{0, 2, 3, 1, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	vertices, indexes := GenerateVertices(atlas, locx, locy, 100, 100)
+
+	op := &ebiten.DrawTrianglesOptions{}
+	for _, triangle := range vertices {
+		screen.DrawTriangles(triangle, indexes, emptyImage, op)
+	}
+
+	return nil
+}
---
+++ squares.go
@@ -0,0 +1,98 @@
+package main
+
+import "github.com/hajimehoshi/ebiten"
+
+// Point contains x and y axis
+type Point struct {
+	X, Y float32
+}
+
+func triangle(p1, p2, p3 Point) []ebiten.Vertex {
+	return []ebiten.Vertex{
+		ebiten.Vertex{DstX: p1.X, DstY: p1.Y, SrcX: 0, SrcY: 0, ColorR: 1, ColorG: 1, ColorB: 1, ColorA: 1},
+		ebiten.Vertex{DstX: p2.X, DstY: p2.Y, SrcX: 0, SrcY: 0, ColorR: 1, ColorG: 1, ColorB: 1, ColorA: 1},
+		ebiten.Vertex{DstX: p3.X, DstY: p3.Y, SrcX: 0, SrcY: 0, ColorR: 1, ColorG: 1, ColorB: 1, ColorA: 1},
+	}
+}
+
+// GenerateVertices generates map of vertices
+func GenerateVertices(fieldmap [][]int, x, y, w, h float32) (vertices [][]ebiten.Vertex, triIndex []uint16) {
+	for i, horizontal := range fieldmap {
+		for j, vertical := range horizontal {
+			vertices = append(vertices, setup(vertical, x+float32(j)*w, y+float32(i)*h, w, h)...)
+		}
+	}
+	return vertices, []uint16{0, 1, 2, 1, 0, 2}
+}
+
+func setup(layout int, x, y, w, h float32) (layouts [][]ebiten.Vertex) {
+	toplef := Point{X: x, Y: y}
+	topmid := Point{X: x + w/2, Y: y}
+	toprgt := Point{X: x + w, Y: y}
+	ctrlef := Point{X: x, Y: y + h/2}
+	ctrrgt := Point{X: x + w, Y: y + h/2}
+	botlef := Point{X: x, Y: y + h}
+	botmid := Point{X: x + w/2, Y: y + h}
+	botrgt := Point{X: x + w, Y: y + h}
+
+	switch layout {
+	case 0:
+	case 1:
+		layouts = append(layouts, triangle(toplef, topmid, ctrlef))
+	case 2:
+		layouts = append(layouts, triangle(topmid, toprgt, ctrrgt))
+	case 3:
+		layouts = append(layouts, triangle(toplef, toprgt, ctrrgt))
+		layouts = append(layouts, triangle(toplef, ctrrgt, ctrlef))
+	case 4:
+		layouts = append(layouts, triangle(ctrrgt, botrgt, botmid))
+	case -5:
+		layouts = append(layouts, triangle(toplef, topmid, ctrlef))
+		layouts = append(layouts, triangle(ctrrgt, botrgt, botmid))
+	case 5:
+		layouts = append(layouts, triangle(topmid, toprgt, ctrrgt))
+		layouts = append(layouts, triangle(topmid, ctrrgt, botmid))
+		layouts = append(layouts, triangle(topmid, botmid, botlef))
+		layouts = append(layouts, triangle(topmid, botlef, ctrlef))
+	case 6:
+		layouts = append(layouts, triangle(topmid, toprgt, botrgt))
+		layouts = append(layouts, triangle(topmid, botrgt, botmid))
+	case 7:
+		layouts = append(layouts, triangle(toplef, toprgt, ctrrgt))
+		layouts = append(layouts, triangle(toplef, ctrrgt, botrgt))
+		layouts = append(layouts, triangle(toplef, botrgt, botmid))
+		layouts = append(layouts, triangle(toplef, botmid, ctrlef))
+	case 8:
+		layouts = append(layouts, triangle(ctrlef, botmid, botlef))
+	case 9:
+		layouts = append(layouts, triangle(toplef, topmid, botmid))
+		layouts = append(layouts, triangle(toplef, botmid, botlef))
+	case -10:
+		layouts = append(layouts, triangle(topmid, toprgt, ctrrgt))
+		layouts = append(layouts, triangle(ctrlef, botmid, botlef))
+	case 10:
+		layouts = append(layouts, triangle(toplef, topmid, ctrrgt))
+		layouts = append(layouts, triangle(toplef, ctrrgt, botrgt))
+		layouts = append(layouts, triangle(toplef, botrgt, botmid))
+		layouts = append(layouts, triangle(toplef, botmid, ctrlef))
+	case 11:
+		layouts = append(layouts, triangle(toplef, toprgt, ctrrgt))
+		layouts = append(layouts, triangle(toplef, ctrrgt, botmid))
+		layouts = append(layouts, triangle(toplef, botmid, botlef))
+	case 12:
+		layouts = append(layouts, triangle(ctrlef, ctrrgt, botrgt))
+		layouts = append(layouts, triangle(ctrlef, botrgt, botlef))
+	case 13:
+		layouts = append(layouts, triangle(toplef, topmid, ctrrgt))
+		layouts = append(layouts, triangle(toplef, ctrrgt, botrgt))
+		layouts = append(layouts, triangle(toplef, botrgt, botlef))
+	case 14:
+		layouts = append(layouts, triangle(topmid, toprgt, botrgt))
+		layouts = append(layouts, triangle(topmid, botrgt, botlef))
+		layouts = append(layouts, triangle(topmid, botlef, ctrlef))
+	case 15:
+		layouts = append(layouts, triangle(toplef, toprgt, botrgt))
+		layouts = append(layouts, triangle(toplef, botrgt, botlef))
+	}
+	return
+}
